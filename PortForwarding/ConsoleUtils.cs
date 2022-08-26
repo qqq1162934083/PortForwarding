5753cd81ff2855018ca8ba210d2b33b1f762dc68
刘华栋 <1162934083@qq.com>
2022-08-26 17:32:23 +0800

    单元测试和Cmd命令优化

--- PortForwarding/ConsoleUtils.cs
+++ PortForwarding/ConsoleUtils.cs
@@ -12,40 +12,81 @@ namespace MyTool
 {
     public class ConsoleUtils
     {
+        /// <summary>
+        /// 通过bat的方式运行cmd获取cmd的结果
+        /// </summary>
+        /// <param name="cmd"></param>
+        /// <returns></returns>
+        /// <exception cref="CmdProcessException"></exception>
+        public static string RunCmd(string cmd)
+        {
+            var nowTime = DateTime.Now;
+            var showDialog = false;//是否显示窗口运行
+            var batPath = Path.Combine(new FileInfo(Assembly.GetExecutingAssembly().Location).Directory.FullName, Guid.NewGuid().ToString("N") + ".bat");
+            var batContentBuilder = new StringBuilder();
+            if (!showDialog) batContentBuilder.AppendLine("@echo off");
+            batContentBuilder.Append(cmd);
+
+            try
+            {
+                using (var writer = File.CreateText(batPath))
+                {
+                    var batContent = batContentBuilder.ToString();
+                    writer.Write(batContent);
+                }
+                var result = (string)null;
+
+                using (var cmdProcess = new Process())
+                {
+                    var redirectInput = true;
+                    cmdProcess.StartInfo.FileName = batPath;//"cmd.exe";//设定程序名
+                    cmdProcess.StartInfo.UseShellExecute = false; //关闭Shell的使用
+                    cmdProcess.StartInfo.RedirectStandardInput = redirectInput;//重定向标准输入
+                    cmdProcess.StartInfo.RedirectStandardOutput = !showDialog;//重定向标准输出
+                    cmdProcess.StartInfo.RedirectStandardError = !showDialog;//重定向错误输出
+                    cmdProcess.StartInfo.CreateNoWindow = !showDialog;//设置不显示窗口
+                    cmdProcess.Start();
+                    if (!showDialog)
+                    {
+                        var successOutputStr = cmdProcess.StandardOutput.ReadToEnd();
+                        var errorOutputStr = cmdProcess.StandardError.ReadToEnd();
+                        if (errorOutputStr.Length > 0) throw new CmdProcessException(errorOutputStr);
+                        result = successOutputStr;
+                    }
+                    cmdProcess.WaitForExit();//等待程序执行完退出进程   
+                    cmdProcess.Close();//结束
+                }
+
+                return result;
+            }
+            finally
+            {
+                if (File.Exists(batPath))
+                    FileSystem.DeleteFile(batPath, UIOption.OnlyErrorDialogs, RecycleOption.SendToRecycleBin);
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="cmd"></param>
+        /// <returns></returns>
         public static string GetCmdResult(string cmd)
         {
-            var guid = Guid.NewGuid().ToString("N");
-            var batPath = Path.Combine(new FileInfo(Assembly.GetExecutingAssembly().Location).Directory.FullName, guid + ".bat");
-            var batContentBuilder = new StringBuilder("@echo off\r\n");
-            batContentBuilder.Append(cmd);
-            using (var writer = File.CreateText(batPath))
+            try
             {
-                var batContent = batContentBuilder.ToString();
-                writer.Write(batContent);
+                return RunCmd(cmd);
             }
-            var nowTime = DateTime.Now;
-            var result = (string)null;
-            using (var cmdProcess = new Process())
+            catch (CmdProcessException exp)
             {
-                var backgrounder = true;
-                var redirectInput = true;
-                cmdProcess.StartInfo.FileName = batPath;//"cmd.exe";//设定程序名
-                cmdProcess.StartInfo.UseShellExecute = false; //关闭Shell的使用
-                cmdProcess.StartInfo.RedirectStandardInput = redirectInput;//重定向标准输入
-                cmdProcess.StartInfo.RedirectStandardOutput = backgrounder;//重定向标准输出
-                cmdProcess.StartInfo.RedirectStandardError = backgrounder;//重定向错误输出
-                cmdProcess.StartInfo.CreateNoWindow = backgrounder;//设置不显示窗口
-                cmdProcess.Start();
-                if (backgrounder)
-                {
-                    result = cmdProcess.StandardOutput.ReadToEnd();
-                    result += cmdProcess.StandardError.ReadToEnd();
-                }
-                cmdProcess.WaitForExit();//等待程序执行完退出进程   
-                cmdProcess.Close();//结束
+                return exp.Message;
             }
-            FileSystem.DeleteFile(batPath, UIOption.OnlyErrorDialogs, RecycleOption.SendToRecycleBin);
-            return result;
+        }
+    }
+    public class CmdProcessException : Exception
+    {
+        public CmdProcessException(string message) : base(message)
+        {
         }
     }
 }
---
+++ PortForwardingTests/ConsoleUtilsTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyTool;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTool.Tests
+{
+    [TestClass()]
+    public class ConsoleUtilsTests
+    {
+        [TestMethod()]
+        public void GetCmdResultTest()
+        {
+            //执行失败
+            var message = ConsoleUtils.GetCmdResult("java");
+            Assert.IsTrue(message.Length > 0);
+        }
+
+        [TestMethod()]
+        public void RunCmdTest()
+        {
+            //执行失败
+            Assert.ThrowsException<CmdProcessException>(() =>
+            {
+                ConsoleUtils.RunCmd("java");
+            });
+        }
+    }
+}
